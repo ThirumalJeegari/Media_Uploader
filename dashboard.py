0b289480dadb69a15951ae3ad8a6212d0a301f70
ThirumalJeegari <thirumaljeegari21@gmail.com>
2026-05-21 00:56:55 +0530

    code updated

--- dashboard.py
+++ dashboard.py
@@ -87,7 +87,14 @@ def dashboard():
                     st.audio(row["file_url"])
 
                 elif "pdf" in row["file_type"]:
-                    st.write("PDF file")
+                    st.markdown(f"[📄 Open PDF]({row['file_url']})")
+
+                    st.markdown(
+                        f"""
+                        <iframe src="{row['file_url']}" width="700" height="900"></iframe>
+                        """,
+                        unsafe_allow_html=True
+                    )
 
                 else:
                     st.markdown(f"[Open File]({row['file_url']})")
