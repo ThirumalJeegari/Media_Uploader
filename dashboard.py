6a73592db00e4796ca3a334c532226ecc4ca48c2
ThirumalJeegari <thirumaljeegari21@gmail.com>
2026-05-21 00:59:51 +0530

    code updated

--- dashboard.py
+++ dashboard.py
@@ -87,7 +87,7 @@ def dashboard():
                     st.audio(row["file_url"])
 
                 elif "pdf" in row["file_type"]:
-                    st.write("Uploaded PDF file")
+                    st.write(uploadedFile)
 
                 else:
                     st.markdown(f"[Open File]({row['file_url']})")
