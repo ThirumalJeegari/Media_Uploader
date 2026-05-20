e218373fe31613c72714a8e14b4aa2f952883ca1
ThirumalJeegari <thirumaljeegari21@gmail.com>
2026-05-20 22:27:51 +0530

    auth_plugin = mysql_native_password , add in the mysql_connector_connect

--- db_C.py
+++ db_C.py
@@ -7,6 +7,7 @@ con = mysql.connector.connect(
     password = st.secrets["password"],
     database = st.secrets["database"],
     port = st.secrets["port"],
+    auth_plugin="mysql_native_password",
     ssl_disabled=False
 )
 
