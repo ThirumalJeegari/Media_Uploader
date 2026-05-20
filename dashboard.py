e0ecd1f2e93a9932d413efbf8484be4719a4a897
ThirumalJeegari <thirumaljeegari21@gmail.com>
2026-05-21 00:44:31 +0530

    code updated

--- dashboard.py
+++ dashboard.py
@@ -32,7 +32,7 @@ def dashboard():
             elif "audio" in uploadedFile.type:
                 st.audio(uploadedFile)
             elif "pdf" in row["file_type"]:
-                st.audio(row["file_url"])
+                st.pdf(row["file_url"])
 
             if st.button("Upload File to Cloudinary"):
                 uploaded_cloudinary = cloudinary.uploader.upload(uploadedFile,resource_type="auto")
@@ -87,7 +87,7 @@ def dashboard():
                     st.audio(row["file_url"])
 
                 elif "pdf" in row["file_type"]:
-                    st.audio(row["file_url"])
+                    st.pdf(row["file_url"])
 
                 else:
                     st.markdown(f"[Open File]({row['file_url']})")
